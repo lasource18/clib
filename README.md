eab9abbd1f367ebbd69afb2782ad9b92d3f535f8
lasource18 <cmguinan@yahoo.fr>
2024-08-01 11:36:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clib
+
+A repository of various programs ranging from data structures and algorithms (solutions to coding challenges) to networking programs and more written in C language.
---
+++ hackerrank/medium/reverse_doubly_linked_list/dllist.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct LinkedListNode
+{
+    int data;
+    struct LinkedListNode* next;
+    struct LinkedListNode* prev;
+};
+
+struct LinkedListNode* reverse(struct LinkedListNode** head) {
+    struct LinkedListNode* curr = *head;
+    struct LinkedListNode* prev = NULL;
+    struct LinkedListNode* next;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        curr->prev = next;
+        prev = curr;
+        curr =  next;
+    }
+
+    *head = prev;
+    
+    return *head;
+}
+
+void printLinkedList(struct LinkedListNode* head) {
+    struct LinkedListNode* current = head;
+    while (current != NULL)
+    {
+        printf("%d <-> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void freeLinkedList(struct LinkedListNode* head) {
+    struct LinkedListNode* current = head;
+    struct LinkedListNode* next;
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    struct LinkedListNode* head = NULL;
+    struct LinkedListNode* first = (struct LinkedListNode*) malloc(sizeof(struct LinkedListNode));
+    first->data = 1;
+    first->next = *(&head);
+    first->next = NULL;
+    *(&head) = first;
+    struct LinkedListNode* current = head;
+
+    for (int i = 2; i < 4; i++)
+    {
+        struct LinkedListNode* node = (struct LinkedListNode*) malloc(sizeof(struct LinkedListNode));
+        node->data = i;
+        node->next = NULL;
+        node->prev = current;
+        current->next = *(&node);
+        current = current->next;
+    }
+
+    printf("Before reverse: ");
+    printLinkedList(head);
+
+    printf("After reverse: ");
+    head = reverse(&head);
+    printLinkedList(head);
+
+    freeLinkedList(head);
+    
+    return 0;
+}
+
